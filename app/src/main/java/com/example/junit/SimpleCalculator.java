5d720010d6d8ad7af8c34ed654754e1d11de96b0
ichierevans <120289420+ichierevans@users.noreply.github.com>
2023-11-02 13:09:29 +0700

    Update SimpleCalculator.java

--- app/src/main/java/com/example/junit/SimpleCalculator.java
+++ app/src/main/java/com/example/junit/SimpleCalculator.java
@@ -1,8 +1,16 @@
 package com.example.junit;
 
 public class SimpleCalculator {
+    /**
+     * Menjumlahkan dua bilangan bulat.
+     *
+     * @param numberA Bilangan bulat pertama yang akan dijumlahkan.
+     * @param numberB Bilangan bulat kedua yang akan dijumlahkan.
+     * @return Hasil penjumlahan dari numberA dan numberB.
+     */
     public static int add(int numberA, int numberB){
-        return numberA+numberB;
+        // Menggunakan operator + untuk menjumlahkan numberA dan numberB.
+        return numberA + numberB;
     }
-
 }
+
